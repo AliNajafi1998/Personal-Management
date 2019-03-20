da2f76e4a699e7128b8775643466ab2e18df8c13
alinajafi <aaaalinajafi1377@gmail.com>
2019-03-20 12:14:04 +0330

    Handling the classes

--- app/src/main/java/com/najafi/ali/personalmanagment/Activities/LoginPage/LoginActivity.java
+++ app/src/main/java/com/najafi/ali/personalmanagment/Activities/LoginPage/LoginActivity.java
@@ -1,20 +1,15 @@
-package com.najafi.ali.personalmanagment.LoginPage;
+package com.najafi.ali.personalmanagment.Activities.LoginPage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.najafi.ali.personalmanagment.Activities.MyActivity;
 import com.najafi.ali.personalmanagment.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends MyActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
-
-
-
-
 }
