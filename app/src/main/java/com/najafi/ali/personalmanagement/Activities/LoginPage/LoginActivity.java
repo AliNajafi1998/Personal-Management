80dac0723d31573825deedd15a1e5530ea47fda7
alinajafi <aaaalinajafi1377@gmail.com>
2019-03-23 00:44:50 +0430

    The Login Page is 80% completed

--- app/src/main/java/com/najafi/ali/personalmanagement/Activities/LoginPage/LoginActivity.java
+++ app/src/main/java/com/najafi/ali/personalmanagement/Activities/LoginPage/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.najafi.ali.personalmanagement.Activities.LoginPage;
 
+import android.graphics.Typeface;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.TextInputEditText;
 import android.view.View;
 
 import com.najafi.ali.personalmanagement.Activities.MyActivity;
@@ -18,16 +20,21 @@ public class LoginActivity extends MyActivity {
         super.onCreate(savedInstanceState);
         StatusBar.initStatusBar(this);
         setContentView(R.layout.activity_login);
-        View v = findViewById(R.id.bg_login);
+        TextInputEditText textInputEditText1 = findViewById(R.id.pass);
+        TextInputEditText textInputEditText2 = findViewById(R.id.enter);
+        textInputEditText1.setTypeface(Typeface.createFromAsset(getResources().getAssets(),"fonts/iransansmobile.ttf"));
+        textInputEditText2.setTypeface(Typeface.createFromAsset(getResources().getAssets(),"fonts/iransansmobile.ttf"));
 
-        AnimationDrawable animationDrawable = (AnimationDrawable) v.getBackground();
-
-        animationDrawable.setEnterFadeDuration(2500);
-        animationDrawable.setExitFadeDuration(5000);
-
-        animationDrawable.start();
+//        animation();
     }
 
+    private void animation() {
+        View v = findViewById(R.id.bg_login);
+        AnimationDrawable animationDrawable = (AnimationDrawable) v.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
 
 
 }
